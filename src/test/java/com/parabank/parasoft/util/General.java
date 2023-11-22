e967c81b9f93797212529db851382481b2dfa151
Hiro Mia <hiromia006@gmail.com>
2023-11-22 21:53:47 +0600

    POM with DDT

--- src/test/java/com/parabank/parasoft/util/General.java
+++ src/test/java/com/parabank/parasoft/util/General.java
@@ -1,5 +1,13 @@
 package com.parabank.parasoft.util;
 
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class General {
     final public static int WAIT_TIME = 30;
 
@@ -9,5 +17,34 @@ public class General {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+
+    public static Object[][] getTestData(String sheetName) {
+        String path = System.getProperty("user.dir") + "/src/test/java/com/parabank/parasoft/pom/testdata/ddt.xlsx";
+        Workbook book = null;
+        Sheet sheet;
+        FileInputStream file = null;
+        try {
+            file = new FileInputStream(path);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            book = WorkbookFactory.create(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        sheet = book.getSheet(sheetName);
+        Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+        // System.out.println(sheet.getLastRowNum() + "--------" +
+        // sheet.getRow(0).getLastCellNum());
+        for (int i = 0; i < sheet.getLastRowNum(); i++) {
+            for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
+                data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
+                System.out.println(data[i][k]);
+            }
+        }
+        return data;
     }
 }
